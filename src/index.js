3b1a8aefacc17d4fbac604b5cfc026d109f22e41
Anton Lapshin <anton.lapshin.s@gmail.com>
2019-05-05 09:25:06 +0300

    Format .jsx files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Main from './components/main/main.jsx';
+import React from "react";
+import ReactDOM from "react-dom";
+import Main from "./components/main/main.jsx";
 
 const init = () => {
-  ReactDOM.render(
-      <Main />,
-      document.getElementById(`root`)
-  );
+  ReactDOM.render(<Main />, document.getElementById(`root`));
 };
 
 init();
