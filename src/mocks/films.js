80284df532dc309b0f6f30dd686da65e8cbfbd57
Anton Lapshin <anton.lapshin.s@gmail.com>
2019-05-25 13:43:41 +0300

    Add new mock data

--- src/mocks/films.js
+++ src/mocks/films.js
@@ -2,26 +2,31 @@ export default [
   {
     id: 1,
     title: `Fantastic Beasts: The Crimes of Grindelwald`,
-    imgsrc: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+    imgsrc: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+    previewsrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
   },
   {
     id: 2,
     title: `Bohemian Rhapsody`,
-    imgsrc: `img/bohemian-rhapsody.jpg`
+    imgsrc: `img/bohemian-rhapsody.jpg`,
+    previewsrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
   },
   {
     id: 3,
     title: `Macbeth`,
-    imgsrc: `img/macbeth.jpg`
+    imgsrc: `img/macbeth.jpg`,
+    previewsrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
   },
   {
     id: 4,
     title: `Aviator`,
-    imgsrc: `img/aviator.jpg`
+    imgsrc: `img/aviator.jpg`,
+    previewsrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
   },
   {
     id: 5,
     title: `We need to talk about Kevin`,
-    imgsrc: `img/we-need-to-talk-about-kevin.jpg`
+    imgsrc: `img/we-need-to-talk-about-kevin.jpg`,
+    previewsrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
   },
 ];
