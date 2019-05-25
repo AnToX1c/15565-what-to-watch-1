737e35bd70abae44d5da01ae80f6b45e461b3633
Anton Lapshin <anton.lapshin.s@gmail.com>
2019-05-25 13:47:04 +0300

    Add video-player component

---
+++ src/components/video-player/video-player.jsx
@@ -0,0 +1,49 @@
+import React, {Component, createRef} from "react";
+import PropTypes from "prop-types";
+
+class VideoPlayer extends Component {
+  constructor(props) {
+    super(props);
+
+    this._videoRef = createRef();
+
+    this.state = {
+      isPlaying: props.isPlaying,
+    };
+  }
+
+  render() {
+    const {imgsrc, previewsrc, muted} = this.props;
+
+    return <video
+      width="280"
+      height="175"
+      loop
+      muted={muted}
+      poster={imgsrc}
+      ref={this._videoRef}
+    >
+      <source src={previewsrc}></source>
+    </video>;
+  }
+
+  componentDidUpdate() {
+    const video = this._videoRef.current;
+    if (this.props.isPlaying) {
+      video.play();
+    } else {
+      video.pause();
+      video.currentTime = 0;
+      video.load();
+    }
+  }
+}
+
+VideoPlayer.propTypes = {
+  imgsrc: PropTypes.string.isRequired,
+  previewsrc: PropTypes.string.isRequired,
+  muted: PropTypes.bool.isRequired,
+  isPlaying: PropTypes.bool.isRequired,
+};
+
+export default VideoPlayer;
