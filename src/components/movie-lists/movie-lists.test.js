409e56c61b68c415c0153268944fa382b8b0e5e7
Anton Lapshin <anton.lapshin.s@gmail.com>
2019-05-31 22:27:46 +0300

    Add mock data to the test files from mocks/films.js

--- src/components/movie-lists/movie-lists.test.js
+++ src/components/movie-lists/movie-lists.test.js
@@ -1,27 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import MovieLists from "./movie-lists";
-
-const mock = [
-  {
-    id: 1,
-    title: `Fantastic Beasts: The Crimes of Grindelwald`,
-    imgsrc: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
-    previewsrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
-  },
-  {
-    id: 2,
-    title: `Bohemian Rhapsody`,
-    imgsrc: `img/bohemian-rhapsody.jpg`,
-    previewsrc: `https://upload.wikimedia.org/wikipedia/commons/transcoded/b/b3/Big_Buck_Bunny_Trailer_400p.ogv/Big_Buck_Bunny_Trailer_400p.ogv.360p.webm`
-  },
-  {
-    id: 3,
-    title: `Macbeth`,
-    imgsrc: `img/macbeth.jpg`,
-    previewsrc: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
-  }
-];
+import mock from "../../mocks/films";
 
 it(`MovieLists page correctly renders`, () => {
   const tree = renderer
